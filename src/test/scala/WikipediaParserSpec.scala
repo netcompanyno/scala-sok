d0d9ad6d4198a8bd0b365bc3b4f407db8e88e32e
Daniel Rødskog <danielr@mesan.no>
2015-03-19 18:11:15 +0100

    Test case for å sjekke at hele wikipedia.xml blir parset

--- src/test/scala/WikipediaParserSpec.scala
+++ src/test/scala/WikipediaParserSpec.scala
@@ -13,4 +13,20 @@ class WikipediaParserSpec extends FlatSpec with Matchers {
     document.title should be ("Det norske Arbeiderparti")
     document.text should startWith ("#REDIRECT [[Arbeiderpartiet]]")
   }
+
+  it should "parse the whole shebang" in {
+    val parser: WikipediaParser = new WikipediaParser()
+
+    val documents: Seq[Document] = parser.parse("src/main/resources/wikipedia.xml")
+
+    documents.size should be (588)
+    val document: Document = documents.head
+    document.id should be (1)
+    document.title should be ("Det norske Arbeiderparti")
+
+    val lastDocument: Document = documents.last
+    lastDocument.id should be (909)
+    lastDocument.title should be ("Roger Ruud")
+
+  }
 }
